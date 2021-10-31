83d8014c486d2f8576632ef8f89798a9cee510df
Akdas <ahmadakdas07@gmail.com>
2021-10-31 21:17:49 +0530

    comments added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+# no need to add any custom model
+# if we have to do user profiling like adding dp, address fields or any other info
+# then we can create a profile model connect it to django's user model
